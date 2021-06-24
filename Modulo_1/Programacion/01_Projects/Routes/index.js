411ff91a6037b29a4eba4af5d7470198412d53df
Jhony Hurtado <jhonyhurtado86@gmail.com>
2021-06-24 18:54:41 -0500

    05 Routers

---
+++ Modulo_1/Programacion/01_Projects/Routes/index.js
@@ -0,0 +1,14 @@
+//Traemos el paquete
+const { Router } = require('express');
+
+//Iniciando Router
+const router = Router()
+
+//creamos los end point
+
+module.exports.RouterIndex = router
+//module = {
+//    exports:{
+//        RouterIndex:router
+//    }
+//}
--- Modulo_1/Programacion/01_Projects/index.js
+++ Modulo_1/Programacion/01_Projects/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
-
 // genero una aplicacion express
 const app = express()
-
+// End point
 app.get('/',(req, res) =>{
     res.send("Hola mundo con express3s")
 })
+
+app.get('/saludo', (req, res) =>{
+    // req.query
+    //console.log(req.query);
+    const {query} =req;
+
+    //const nombre = req.query.nombre
+    //const apellido = req.query.apellido
+
+    res.json({
+        saludo:`Hola soy ${query.nombre} ${query.apellido}`,
+    });
+});
+
+
+app.get('/saludo/:nombre', (req, res) => {
+    const { params : { nombre } } = req
+    res.json({
+        nombre:nombre
+    });
+});
 
 app.listen(3000,() => {
     console.log("servidor escuchando en htpp://localhost:3000");
